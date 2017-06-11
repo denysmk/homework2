f0cf6b46a1d4d50808da9f995afd83087fd18b58
Denys Makarenko <makarenko.denys@gmail.com>
2017-06-11 23:09:48 +0300

    Second part of task (state_and_behaviour) was implemented

--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
-# exercise state and behaviour
 module StateAndBehaviour
+  # define class Car
+  class Car
+    attr_accessor :current_speed
+    attr_reader :year, :color, :model
+
+    def self.default_car
+      new
+    end
+
+    def initialize(car_attributes)
+      @year = car_attributes.fetch(:year, '2003')
+      @color = car_attributes.fetch(:color, 'red')
+      @model = car_attributes.fetch(:model, 'coupe')
+      @current_speed = 0
+    end
+
+    def speed_up(speed)
+      self.current_speed += speed
+    end
+
+    def push_break(speed)
+      self.current_speed -= speed if speed < current_speed
+    end
+  end
 end
